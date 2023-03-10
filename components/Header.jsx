36b935678cc234147ab4f91c97b73b89a78bca82
nandantyagi <nandantyagi@gmail.com>
2023-03-10 01:01:11 +0100

    Initail commit 1

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <nav>
             <div className={styles.header}>
                 <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_609_2)">
+                    <g clipPath="url(#clip0_609_2)">
                         <rect width="48" height="48" fill="white" />
                         <rect x="0.5" y="0.5" width="47" height="47" fill="black" stroke="#000" />
                         <circle cx="24" cy="24" r="21" fill="#FFFEFE" />
